409e57854327970350403b2792407893926d7f7d
denismaster <denismaster@outlook.com>
2015-12-13 21:12:01 +0300

    Initial project commit

---
+++ dotnet01/dotnet01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(dotnet01.Startup))]
+namespace dotnet01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
