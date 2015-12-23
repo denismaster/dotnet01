fcf030d9b7453b29c21cfcdd1867f0d05757502a
denismaster <denismaster@outlook.com>
2015-12-23 22:42:00 +0300

    Account Stub

---
+++ dotnet01/dotnet01/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dotnet01.Areas.Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+        //TODO:define IAccountRepository here
+        public AccountController()
+        {
+            //TODO:init repository with our implementation
+        }
+        public ActionResult New()
+        {
+            //Create AccountNewViewModel and send it to View via parameter
+            return View();
+        }
+        public ActionResult Edit(int id)
+        {
+            //Create AccountEditViewModel and send it to View via parameter
+            //Actions are similar, can use only one model
+            return View();
+        }
+        public ActionResult Index()
+        {
+            //Create AccountIndexViewModel and send it to View via parameter
+            return View();
+        }
+    }
+}
