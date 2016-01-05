efc39c3c1228748683a531c1c8228fd6f631e3a4
Ismail <ismailablyazzizov@gmail.com>
2016-01-05 19:04:16 +0300

    Добавлен контроллер к Course + View "course list"

---
+++ dotnet01/dotnet01/Areas/Manager/Controllers/CourseController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using dotnet01.Areas.Admin.Models;
+using dotnet01.Areas.Manager.Models;
+using System.Data.Entity;
+using dotnet01.Areas.Admin.Controllers;
+using Courses.Buisness.Account;
+using Models2 = Courses.Models;
+
+
+namespace dotnet01.Areas.Manager.Controllers
+{
+    public class CourseController : Controller
+    {
+        AccountContext db = new AccountContext();
+
+        public ActionResult Index()
+        {
+            IEnumerable<Course> courses = db.Courses;
+            ViewBag.Courses = courses;
+            return View();
+        }
+
+
+    }
+}
