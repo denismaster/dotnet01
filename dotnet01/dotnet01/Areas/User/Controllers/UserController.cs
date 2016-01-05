3aa25d3e96d0c1b5276910f114e050c0fb453937
Никулин Константин <nikulin.konstantinv@gmail.com>
2016-01-05 11:19:06 +0300

    add user

---
+++ dotnet01/dotnet01/Areas/User/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using dotnet01.Areas.Admin.Models;
+using System.Data.Entity;
+using dotnet01.Areas.Admin.Controllers;
+using Courses.Buisness.Account;
+using Models2 = Courses.Models;
+namespace dotnet01.Areas.User.Controllers
+{
+
+    public class UserController : Controller
+    {
+        
+
+    }
+}
