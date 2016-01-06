c10778c947780bae56acd6d6e993e3230318f00a
Red Fox <redfox9966@gmail.com>
2016-01-06 18:38:23 +0300

    new course action added (without validation)

--- dotnet01/dotnet01/Areas/Manager/Controllers/CourseController.cs
+++ dotnet01/dotnet01/Areas/Manager/Controllers/CourseController.cs
@@ -6,7 +6,6 @@ using System.Web.Mvc;
 using dotnet01.Areas.Admin.Models;
 using dotnet01.Areas.Manager.Models;
 using System.Data.Entity;
-using dotnet01.Areas.Admin.Controllers;
 using Courses.Buisness.Account;
 using Models2 = Courses.Models;
 
@@ -16,7 +15,6 @@ namespace dotnet01.Areas.Manager.Controllers
     public class CourseController : Controller
     {
         AccountContext db = new AccountContext();
-
         public ActionResult Index()
         {
             IEnumerable<Course> courses = db.Courses;
@@ -30,5 +28,58 @@ namespace dotnet01.Areas.Manager.Controllers
             ViewBag.Courses = courses;
             return View();
         }
+
+        [HttpGet]
+        public ActionResult New()
+        {
+           Course course = new Course();
+           CourseNewViewModel model = new CourseNewViewModel() { Course=course };
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult New(Course course)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (course.Dates == null)
+                    {
+                        course.Dates = "";
+                    }
+                    if (course.Location == null)
+                    {
+                        course.Location = "";
+                    }
+                    if (course.Teacher == null)
+                    {
+                        course.Teacher = "";
+                    }
+                    if (course.Organizer == null)
+                    {
+                        course.Organizer = "";
+                    }
+                    if (course.Description == null)
+                    {
+                        course.Description = "";
+                    }
+                    if (course.Image == null)
+                    {
+                        course.Image = null;
+                    }
+                    db.Courses.Add(course);
+                    db.SaveChanges();
+                    return RedirectToAction("ManagerPage");
+
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Write(e);
+                ModelState.AddModelError("", "Unable to save changes");
+            }
+            return View();
+        }
+
     }
 }
---
+++ dotnet01/dotnet01/Areas/Manager/Models/CourseNewViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dotnet01.Areas.Manager.Models
+{
+    public class CourseNewViewModel
+    {
+        public Course Course { get; set; }
+    }
+}
