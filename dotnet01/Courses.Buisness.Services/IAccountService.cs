be39ed4341b985237298d9c95f00650b632654ba
denismaster <denismaster@outlook.com>
2016-01-30 02:44:28 +0300

    Assembly decomposition

---
+++ dotnet01/Courses.Buisness.Services/IAccountService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Courses.Models;
+using Courses.ViewModels;
+namespace Courses.Buisness
+{
+    public interface IAccountService
+    {
+        /// <summary>
+        /// Возвращает список аккаунтов. 
+        /// TODO:Желательно возвращать готовые ViewModels, но это пока неважно.
+        /// </summary>
+        AccountCollectionViewModel GetAccounts(int page, int pageSize,
+            List<Filtering.FieldFilter> fieldFilter = null, Filtering.SortFilter sortFilter = null);
+        /// <summary>
+        /// Получение одного аккаунта
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        AccountViewModel GetByID(int id);
+        /// <summary>
+        /// Добавление аккаунта. 
+        /// </summary>
+        /// <param name="account"></param>
+        void Add(AccountViewModel account);
+        /// <summary>
+        /// Обновление данных аккаунта
+        /// </summary>
+        /// <param name="account"></param>
+        void Edit(AccountViewModel account);
+        /// <summary>
+        /// Удаление аккаунта
+        /// </summary>
+        /// <param name="account"></param>
+        void Delete(AccountViewModel account);
+        /// <summary>
+        /// Сохранение изменений в репозитории
+        /// </summary>
+        void SaveChanges();
+    }
+}
--- dotnet01/Courses.Buisness/Account/AccountService.cs
+++ dotnet01/Courses.Buisness/Account/AccountService.cs
@@ -8,40 +8,7 @@ using Courses.Models.Repositories;
 using Courses.ViewModels;
 namespace Courses.Buisness
 {
-    public interface IAccountService
-    {
-        /// <summary>
-        /// Возвращает список аккаунтов. 
-        /// TODO:Желательно возвращать готовые ViewModels, но это пока неважно.
-        /// </summary>
-        AccountCollectionViewModel GetAccounts(int page, int pageSize,
-            List<Filtering.FieldFilter> fieldFilter = null, Filtering.SortFilter sortFilter = null);
-        /// <summary>
-        /// Получение одного аккаунта
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        AccountViewModel GetByID(int id);
-        /// <summary>
-        /// Добавление аккаунта. 
-        /// </summary>
-        /// <param name="account"></param>
-        void Add(AccountViewModel account);
-        /// <summary>
-        /// Обновление данных аккаунта
-        /// </summary>
-        /// <param name="account"></param>
-        void Edit(AccountViewModel account);
-        /// <summary>
-        /// Удаление аккаунта
-        /// </summary>
-        /// <param name="account"></param>
-        void Delete(AccountViewModel account);
-        /// <summary>
-        /// Сохранение изменений в репозитории
-        /// </summary>
-        void SaveChanges();
-    }
+    
     public class AccountService : IAccountService
     {
         /// <summary>
