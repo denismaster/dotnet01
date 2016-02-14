4c3af4a1bd36f38945424c47181e6ae13e3e8ad7
DmitrySpor <dmitrysporish@gmail.com>
2016-02-14 14:35:45 +0300

    small commit

---
+++ dotnet01/Courses.Gui.Client/ClientControllerFactory.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Ninject;
+using Courses.Models;
+using Courses.Models.Repositories;
+using Courses.Buisness;
+using Courses.DAL;
+using Courses.Buisness.Filtering;
+using Courses.Buisness.Services;
+
+namespace Courses.Gui.Client
+{
+    /// <summary>
+    /// Фабрика, которая будет управлять нашим ManagerController
+    /// </summary>
+    public class ClientControllerFactory : System.Web.Mvc.DefaultControllerFactory
+    {
+        /// <summary>
+        /// Ядро Ninject
+        /// </summary>
+        IKernel kernel;
+
+        public ClientControllerFactory()
+        {
+            kernel = new StandardKernel();
+            AddBindings();
+        }
+        /// <summary>
+        /// Получаем контроллер, делая resolve зависимостей с Ninject
+        /// </summary>
+        /// <param name="requestContext"></param>
+        /// <param name="controllerType"></param>
+        /// <returns></returns>
+        protected override System.Web.Mvc.IController GetControllerInstance(System.Web.Routing.RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null
+                  ? null
+                  : (System.Web.Mvc.IController)kernel.Get(controllerType);
+        }
+        /// <summary>
+        /// Настраиваем контейнер
+        /// </summary>
+        //TODO Использовать авторегистрацию в других фабриках, это на будущее
+        private void AddBindings()
+        {
+            kernel.Bind<IProductService>().To<ProductService>();
+            kernel.Bind<IFilterFactory<Product>>().To<ProductFilterFactory>();
+            kernel.Bind<IProductRepository>().To<ProductRepository>();
+
+            kernel.Bind<IPartnerRepository>().To<PartnerRepository>();
+            kernel.Bind<IPartnerService>().To<PartnerService>();
+
+            kernel.Bind<IAccountRepository>().To<AccountRepository>();
+
+            kernel.Bind<IFilterFactory<Partner>>().To<PartnerFilterFactory>();
+        }
+    }
+}
---
+++ dotnet01/Courses.Gui.Client/Controllers/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Courses.DAL;
+using Courses.ViewModels;
+using Courses.Buisness.Services;
+using Courses.Buisness.Filtering;
+
+namespace Courses.Gui.Client.Controllers
+{
+    public class ProductController : ApiController
+    {
+        private readonly IProductService productService;
+
+        public ProductController(IProductService productService)
+        {
+            if (productService == null)
+                throw new ArgumentNullException();
+            this.productService = productService;
+        }
+        // GET: api/Product
+        public ProductCollectionViewModel GetProducts()
+        {
+            int? page = 1;
+            int pageSize = 3;
+            int currentPage = page ?? 1;
+            var products = productService.GetProducts(currentPage, pageSize);
+            return products;
+        }
+
+        // GET: api/Product/5
+        [ResponseType(typeof(ProductViewModel))]
+        public async Task<IHttpActionResult> GetProduct(int id)
+        {
+            var productView = productService.GetProductWithAccauntsAndPartners(id);
+            if (productView.Id == 0)
+                return NotFound();
+            return Ok(productView);
+        }
+    }
+}
