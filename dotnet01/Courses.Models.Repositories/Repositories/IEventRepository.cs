c8243cba788eaea242aa44c99a23bc2359b67a80
KirillMaster <suhorukih@mail.ru>
2016-03-03 21:57:40 +0300

    Interfaces of repositories

---
+++ dotnet01/Courses.Models.Repositories/Repositories/IEventRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Courses.Models.Repositories.Repositories
+{
+   interface IEventRepository
+    {
+        IEnumerable<Models.Comment> Get();
+
+        IEnumerable<Models.Comment> Get(int page, int pageSize, Func<Models.Comment, bool> expression);
+
+        Models.Comment Get(int id);
+
+        void Add(Models.Comment entity);
+
+        void Update(Models.Comment entity);
+
+        void Delete(Models.Comment entity);
+
+
+        void SaveChanges();
+
+
+
+        int Count(Func<Models.Comment, bool> expression);
+
+
+        void Dispose();
+    }
+}
