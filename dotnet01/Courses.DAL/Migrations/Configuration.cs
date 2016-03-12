e53d31d2acde1e5bc5042e5ee9c28e9f4618b0db
DmitrySpor <tankikaki@yandex.ru>
2016-03-12 08:57:57 +0300

    Update database with picturepath for Products.

---
+++ dotnet01/Courses.DAL/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace Courses.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Courses.DAL.DatabaseContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Courses.DAL.DatabaseContext";
+        }
+
+        protected override void Seed(Courses.DAL.DatabaseContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
