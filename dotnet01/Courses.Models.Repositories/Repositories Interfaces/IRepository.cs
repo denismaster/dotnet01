0f527938ee716b1f05c93b0e046dde48b0c009d6
KirillMaster <suhorukih@mail.ru>
2016-03-21 22:52:25 +0300

    fix empty methods

--- dotnet01/Courses.Models.Repositories/Repositories Interfaces/IRepository.cs
+++ dotnet01/Courses.Models.Repositories/Repositories Interfaces/IRepository.cs
@@ -59,6 +59,6 @@ namespace Courses.Models.Repositories
         /// <summary>
         /// удаляет все данные о сущностях данного типа из БД
         /// </summary>
-        
+        T GetOnlyOne();
     }
 }
