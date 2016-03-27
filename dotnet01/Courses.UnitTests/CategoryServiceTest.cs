051724f2ddb9911f4388147a82e60783d5f71fc5
Alexander Undin <dorian.adret@gmail.com>
2016-03-27 12:03:48 +0300

    Add implementation for CategoryServiceTest

--- dotnet01/Courses.UnitTests/CategoryServiceTest.cs
+++ dotnet01/Courses.UnitTests/CategoryServiceTest.cs
@@ -3,35 +3,91 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Courses.ViewModels;
 using NUnit.Framework;
+using Courses.Buisness;
+using Courses.DAL;
+using Courses.Buisness.Filtering;
+using Courses.ViewModels;
+using Courses.Buisness.Services;
 
 namespace Courses.UnitTests
 {
     [TestFixture]
     public class CategoryServiceTest
     {
-        public void GetCategorysTest(int page, int pageSize,
-           List<Buisness.Filtering.FieldFilter> fieldFilter = null, Buisness.Filtering.SortFilter sortFilter = null)
+        [Test]
+        public void GetCategorysTest()
         {
+            ICategoryService _categoryService = new CategoryService(new CategoryRepository(), new CategoryFilterFactory());
+            CategoryCollectionViewModel actual = _categoryService.GetCategorys(1, 5);
+            Assert.IsNotNull(actual);
         }
-        public void GetCategoryWithCategorysTest(int? Id)
+
+        [Test]
+        public void GetCategoryWithCategorysTest()
         {
+            ICategoryService _categoryService = new CategoryService(new CategoryRepository(), new CategoryFilterFactory());
+            CategoryViewModelForAddEditView actual = _categoryService.GetCategoryWithCategorys(null);
+            Assert.IsNotNull(actual);
         }
-        public void GetByIDTest(int id)
+
+        [Test]
+        public void GetByIDTest()
         {
+            ICategoryService _categoryService = new CategoryService(new CategoryRepository(), new CategoryFilterFactory());
+            CategoryViewModel _categoryViewModel = GetCategoryViewModel();
+            _categoryService.Add(_categoryViewModel);
+            Assert.IsNotNull(_categoryService.GetByID(1));
         }
-        public void AddTest(CategoryViewModel category)
+
+        [Test]
+        public void AddTest()
         {
+            ICategoryService _categoryService = new CategoryService(new CategoryRepository(), new CategoryFilterFactory());
+            CategoryViewModel _categoryViewModel = GetCategoryViewModel();
+            _categoryService.Add(_categoryViewModel);
+            Assert.IsNotNull(_categoryService.GetByID(1));
         }
-        public void EditTest(CategoryViewModel category)
+        [Test]
+        public void EditTest()
         {
+            ICategoryService _categoryService = new CategoryService(new CategoryRepository(), new CategoryFilterFactory());
+            CategoryViewModel _categoryViewModel = GetCategoryViewModel();
+            _categoryService.Add(_categoryViewModel);
+            _categoryViewModel.Description = "otherDescription";
+            _categoryService.Edit(_categoryViewModel);
+            bool actual, expected = true;
+            if(_categoryService.GetByID(1).Description.CompareTo("otherDescription") == 0)
+            {
+                actual = true;
+            }
+            else
+            {
+                actual = false;
+            }
+            Assert.AreEqual(expected, actual);
         }
-        public void DeleteTest(CategoryViewModel category)
+
+        [Test]
+        public void DeleteTest()
         {
+            ICategoryService _categoryService = new CategoryService(new CategoryRepository(), new CategoryFilterFactory());
+            CategoryViewModel _categoryViewModel = GetCategoryViewModel();
+            _categoryService.Add(_categoryViewModel);
+            _categoryService.Delete(_categoryViewModel);
+            Assert.IsNotNull(_categoryService.GetByID(1));
         }
-        public void SaveChangesTest()
+
+        private CategoryViewModel GetCategoryViewModel()
         {
+            CategoryViewModel _categoryViewModel = new CategoryViewModel();
+            _categoryViewModel.Description = "testDecrtioption";
+            _categoryViewModel.Name = "testName";
+            _categoryViewModel.Active = true;
+            _categoryViewModel.CreatedDate = new DateTime(1, 1, 1);
+            _categoryViewModel.UpdatedDate = new DateTime(1, 1, 1);
+            _categoryViewModel.Id = 1;
+            return _categoryViewModel
         }
     }
 }
