bb2f6addf5af543c176f1e2c5cd85332c6f8b229
Alexander Undin <dorian.adret@gmail.com>
2016-03-27 10:36:28 +0300

    Add implementation for AuthenticationServiceTest

--- dotnet01/Courses.UnitTests/AuthenticationServiceTest.cs
+++ dotnet01/Courses.UnitTests/AuthenticationServiceTest.cs
@@ -3,41 +3,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Courses.ViewModels;
-using Courses.Models;
 using NUnit.Framework;
+using Courses.Buisness.Services;
+using Courses.Buisness.Authentication;
+using Courses.DAL;
+using Courses.Models;
 
 namespace Courses.UnitTests
 {
     [TestFixture]
     public class AuthenticationServiceTest
     {
-        public void FindTest(int id)
-        {            
-        }
-        public void FindTest(string username)
+        [Test]
+        public void FindTest()
         {
+            IAuthenticationService _authenticationService = new AuthenticationService(new AccountRepository(), new SHA256PasswordHasher());            
+            bool expected = true, actual;
+            if (_authenticationService.Find(1) != null &&
+                _authenticationService.Find("testName") != null &&
+                _authenticationService.Find("testName", "testPassword") != null)
+            {
+                actual = true;
+            }
+            else
+            {
+                actual = false;
+            }
+            Assert.AreEqual(expected, actual);
         }
-        public void FindTest(string username, string password)
+
+        [Test]
+        public void FindExternalTest()
         {
+            IAuthenticationService _authenticationService = new AuthenticationService(new AccountRepository(), new SHA256PasswordHasher());
+            Assert.IsNotNull(_authenticationService.FindExternal("testAuthKey"));
         }
-        public void FindExternalTest(string authKey)
+
+        [Test]
+        public void GetIdentityTest()
         {
+            IAuthenticationService _authenticationService = new AuthenticationService(new AccountRepository(), new SHA256PasswordHasher());
+            Assert.IsNotNull(_authenticationService.GetIdentity(new User()));
         }
-        public void LinkExternalLoginTest(User user, string authkey, string providername)
+
+        [Test]
+        public void RegisterTest()
         {
+            IAuthenticationService _authenticationService = new AuthenticationService(new AccountRepository(), new SHA256PasswordHasher());
+            bool expected = true, actual;
+            if(_authenticationService.Register("testUserName", "testPassword") &&
+                _authenticationService.Register("testUserNameOther", "testAuthKey", "testProvider"))
+            {
+                actual = true;
+            }
+            else
+            {
+                actual = false;
+            }
+            Assert.AreEqual(expected, actual);
         }
-        public void UnlinkExternalLoginTest(User user)
+
+        private User GetUser()
         {
-        }
-        public void GetIdentityTest(User user)
-        {
-        }
-        public void RegisterTest(string username, string password)
-        {
-        }
-        public void RegisterTest(string username, string authkey, string provider)
-        {
+            User _user = new User();
+            _user.Email = "testc@tect.com";
+            _user.FirstName = "testFirstName";
+            _user.LastName = "testLastName";
+            _user.Login = "testLogin";
+            _user.Role = "Default";
+            _user.CreatedDate = new DateTime(1, 1, 1);
+            _user.UpdatedDate = new DateTime(1, 1, 1);
+            return _user;
         }
     }
 }
