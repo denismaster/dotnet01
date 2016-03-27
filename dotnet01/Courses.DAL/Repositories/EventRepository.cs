694478ab90995dfe89dc94b8081eb3a25db61bda
DmitrySpor <tankikaki@yandex.ru>
2016-03-27 16:23:42 +0300

    Add EventService, OrderSevice, CommentService. Some fix

--- dotnet01/Courses.DAL/Repositories/EventRepository.cs
+++ dotnet01/Courses.DAL/Repositories/EventRepository.cs
@@ -1,14 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Courses.Models;
 using Courses.Models.Repositories;
-using Courses.Models;
+using System;
+using System.Collections.Generic;
 using System.Data.Entity;
+using System.Linq;
 namespace Courses.DAL
 {
-    class EventRepository:RepositoryBase<Event>, IEventRepository
-    { 
+    public class EventRepository : RepositoryBase<Event>, IEventRepository
+    {
+        public IEnumerable<Models.Event> Get(int page, int pageSize, Func<Models.Event, bool> expression, SortFilter sortFilter)
+        {
+            if (string.IsNullOrWhiteSpace(sortFilter.SortOrder))
+            {
+                return entityContext.Where(expression).OrderBy(s => s.Id).Skip((page - 1) * pageSize).Take(pageSize).AsEnumerable();
+            }
+            switch (sortFilter.SortOrder)
+            {
+
+                case "CreatedDate":
+                    return entityContext.Where(expression).OrderBy(s => s.CreatedDate).Skip((page - 1) * pageSize).Take(pageSize).AsEnumerable();
+                case "CreatedDateDesc":
+                    return entityContext.Where(expression).OrderByDescending(s => s.CreatedDate).Skip((page - 1) * pageSize).Take(pageSize).AsEnumerable();
+
+                default:
+                    return entityContext.Where(expression).OrderBy(s => s.Id).Skip((page - 1) * pageSize).Take(pageSize).AsEnumerable();
+            }
+        }
 
     }
 }
--- dotnet01/Courses.DAL/Repositories/OrderRepository.cs
+++ dotnet01/Courses.DAL/Repositories/OrderRepository.cs
@@ -1,14 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Courses.Models;
 using Courses.Models.Repositories;
-using Courses.Models;
+using System;
+using System.Collections.Generic;
 using System.Data.Entity;
+using System.Linq;
 
 namespace Courses.DAL
 {
-    class OrderRepository:RepositoryBase<Order>,IOrderRepository
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
     {
+        public IEnumerable<Models.Order> Get(int page, int pageSize, Func<Models.Order, bool> expression, SortFilter sortFilter)
+        {
+            if (string.IsNullOrWhiteSpace(sortFilter.SortOrder))
+            {
+                return entityContext.Where(expression).OrderBy(s => s.Id).Skip((page - 1) * pageSize).Take(pageSize).AsEnumerable();
+            }
+            switch (sortFilter.SortOrder)
+            {
+
+                case "CreatedDate":
+                    return entityContext.Where(expression).OrderBy(s => s.CreatedDate).Skip((page - 1) * pageSize).Take(pageSize).AsEnumerable();
+                case "CreatedDateDesc":
+                    return entityContext.Where(expression).OrderByDescending(s => s.CreatedDate).Skip((page - 1) * pageSize).Take(pageSize).AsEnumerable();
+
+                default:
+                    return entityContext.Where(expression).OrderBy(s => s.Id).Skip((page - 1) * pageSize).Take(pageSize).AsEnumerable();
+            }
+        }
     }
 }
